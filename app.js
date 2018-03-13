fa49edeaa5410e330d21f0cd75be480a72e6b058
Nico Hickman <nicohman@protonmail.com>
2018-03-13 08:30:40 -0700

    Add basics of youtube bot

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ var io = require('socket.io'),
 	names = ["dragon", "defiant", "dragon's teeth", "saint", "weaver"],
 	semaphore = require('semaphore'),
 	sem = semaphore(1),
+	Youtube = require("youtube-node"),
+	youtube = Youtube(),
 	DEMPATH = "/home/nicohman/.demenses/",
 	san = require("sanitizer"),
 	events = require('events'),
@@ -31,6 +33,7 @@ var io = require('socket.io'),
 	posts = require(DEMPATH + 'posts_' + name + '.json'),
 	users = require(DEMPATH + "users_" + name + ".json"),
 	port = ports[commandArg - 1],
+	ytChannels = ["UURrWaIO7p-1fmJrb95gBZwA"],
 	curations = require(DEMPATH + "curations_" + name + ".json"),
 	secret = config.secret,
 	emailSecret = config.emailSecret,
@@ -49,6 +52,7 @@ var io = require('socket.io'),
 	rec = {},
 	logged = {};
 var https = require("https");
+youtube.setKey(config.yt);
 //Startup logs.
 console.log(
 	"________                                                     \n\\______ \\   ____   _____   ____   ____   ______ ____   ______\n |    |  \\_/ __ \\ /     \\_/ __ \\ /    \\ /  ___// __ \\ /  ___/\n |    `   \\  ___/|  Y Y  \\  ___/|   |  \\___ \\  ___/ \\___ \\ \n/_______  /\\__  >__|_|  /\\___  >___|  /____  >\\___  >____  >\n        \\/     \\/      \\/     \\/     \\/     \\/     \\/     \\/ "
@@ -180,6 +184,44 @@ function fulfill(name, condition, func, auth, amal, easy) {
 	return doneFunc;
 }
 var time = moment(fs.readFileSync("/home/nicohman/.demenses/timer"), 'x');
+var lastYts = require(DEMPATH + "/.demenses/lastyts.json");
+if (!lastYts) {
+	lastYts = {};
+}
+
+function writeLast() {
+	fs.writeFile("/home/nicohman/.demenses/lastyts.json", JSON.stringify(lastYts),
+		function () {})
+}
+
+function checkYt() {
+	ytChannels.forEach(function (item) {
+		youtube.getPlayListsItemsById(item, function (err, res) {
+			if (err) {
+				console.error(err);
+			} else {
+				if (res.items[0]) {
+					console.log(res.items[0]);
+					var uid = "johnnyfiveisalive"
+					var vid = res.items[0].snippet;
+					if (vid.resourceId.videoId.trim() != lastYts[item].trim()) {
+						createPost({
+							title: vid.title,
+							content: "https://youtube.com/watch?v=" + vid.resourceId.videoId,
+							auth: "yt_bot",
+							uid: uid,
+							tags: ["yt_bot", "bot", "videos"]
+						});
+						lastYts[item] = vid.resourceId.videoId;
+						writeLast();
+					} else {
+						console.log("NO NEW VIDEO FOR " + item)
+					}
+				}
+			}
+		});
+	});
+}
 /*var get_posts_top = new indefinite("get_posts_top", function(req){
 	var check = Object.keys(posts).map(function(m) {
 		return posts[m];
@@ -2461,5 +2503,7 @@ function createClient(to_connect) {
 }
 if (process.argv[2] == "1") {
 	setTimeout(checkMe, 1000);
-	setInterval(checkMe, 30000)
+	setTimeout(checkYt, 2000);
+	setInterval(checkYt, 1200000);
+	setInterval(checkMe, 60000)
 }
