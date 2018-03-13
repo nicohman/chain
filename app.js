d705d47e1a5702841076bd407455c870d349b6d6
Nico Hickman <nicohman@protonmail.com>
2018-03-13 08:56:51 -0700

    Basic XKCD Bot

--- app.js
+++ app.js
@@ -184,7 +184,8 @@ function fulfill(name, condition, func, auth, amal, easy) {
 	return doneFunc;
 }
 var time = moment(fs.readFileSync("/home/nicohman/.demenses/timer"), 'x');
-var lastYts = require(DEMPATH + "/.demenses/lastyts.json");
+var lastYts = require("/home/nicohman/.demenses/lastyts.json");
+var lastX = require("/home/nicohman/.demenses/lastX")
 if (!lastYts) {
 	lastYts = {};
 }
@@ -310,6 +311,33 @@ function verify(token, cb) {
 		} else {
 			cb(decode);
 		}
+	});
+}
+
+function checkX() {
+	https.get("https://xkcd.com/info.0.json", function (res) {
+		var data = '';
+		res.on('data', function (bit) {
+			data += bit;
+		});
+		res.on('end', function () {
+			data = JSON.parse(data);
+			if (data.num != lastX) {
+				var uid = ""
+				data = JSON.parse(data);
+				createPost({
+					title: "XKCD #" + data.num + " " + data.title,
+					content: data.img,
+					auth: "xkcd_bot",
+					uid: uid,
+					tags: ["xkcd", "bot", "comics"]
+				});
+				lastX = data.num;
+				fs.writeFile("/home/nicohman/.demenses/lastX", data.num, function () {
+					console.log("XKCD Last updated!");
+				});
+			}
+		})
 	});
 }
 var checkMe = function () {
@@ -1923,7 +1951,7 @@ var serv_handles = {
 		get_curation_posts(req.cur, function (posts) {
 			var got = false;
 			if (!got) {
-				posts =  checkFavs(users[logged[req.cid]].favorites, posts);
+				posts = checkFavs(users[logged[req.cid]].favorites, posts);
 				io.to(req.cid).emit("c_got_cur_posts_" + req.cur + "_" + req.time,
 					posts);
 				console.log("EMITTING EVENT C: " + req.time);
@@ -2353,8 +2381,8 @@ var serv_handles = {
 	},
 	"c_get_post_by_id": function (req) {
 		get_post_by_id(req.pid, function (res) {
-			if(logged[req.cid]){
-				if(users[logged[req.cid]].favorites[req.pid] === true){
+			if (logged[req.cid]) {
+				if (users[logged[req.cid]].favorites[req.pid] === true) {
 					res.favorited = true;
 				}
 			}
@@ -2513,6 +2541,8 @@ function createClient(to_connect) {
 if (process.argv[2] == "1") {
 	setTimeout(checkMe, 1000);
 	setTimeout(checkYt, 2000);
+	setTimeout(checkX, 3000);
+	setInterval(checkX, 1200000)
 	setInterval(checkYt, 1200000);
 	setInterval(checkMe, 60000)
 }
